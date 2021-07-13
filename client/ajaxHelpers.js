236c5524a6776be5c9985d8decffdb494b930395
Mary Gordanier <mcgordanier@gmail.com>
2021-07-13 18:06:55 -0400

    refactor to replace array methods

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
-// Add your cohort name to the cohortName variable below
-const cohortName = ''
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = 'COHORT-NAME';
 // Use the APIURL variable for fetch requests
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
 
-}
+};
 
 export const fetchSinglePlayer = async (playerId) => {
 
-}
+};
 
 export const addNewPlayer = async (playerObj) => {
 
-}
+};
 
 export const removePlayer = async (playerId) => {
 
-}
+};
--- client/renderHelpers.js
+++ client/renderHelpers.js
@@ -1,18 +1,19 @@
-import {fetchAllPlayers} from './ajaxHelpers';
+import { fetchAllPlayers } from './ajaxHelpers';
 
 const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 export const renderAllPlayers = (playerList) => {
   // First check if we have any data before trying to render it!
-  if(!playerList || !playerList.length) {
+  if (!playerList || !playerList.length) {
     playerContainer.innerHTML = '<h3>No players to display!</h3>';
     return;
   }
 
   // Loop through the list of players, and construct some HTML to display each one
   let playerContainerHTML = '';
-  playerList.map(pup => {
+  for (let i = 0; i < playerList.length; i++) {
+    const pup = playerList[i];
     let pupHTML = `
       <div class="single-player-card">
         <div class="header-info">
@@ -24,7 +25,7 @@ export const renderAllPlayers = (playerList) => {
       </div>
     `;
     playerContainerHTML += pupHTML;
-  });
+  }
 
   // After looping, fill the `playerContainer` div with the HTML we constructed above
   playerContainer.innerHTML = playerContainerHTML;
@@ -33,17 +34,18 @@ export const renderAllPlayers = (playerList) => {
   // we want to grab those "See details" buttons on each player
   // and attach a click handler to each one
   let detailButtons = [...document.getElementsByClassName('detail-button')];
-  detailButtons.forEach(button => {
+  for (let i = 0; i < detailButtons.length; i++) {
+    const button = detailButtons[i];
     button.addEventListener('click', async () => {
       /*
         YOUR CODE HERE
       */
     });
-  });
+  }
 };
 
 export const renderSinglePlayer = (playerObj) => {
-  if(!playerObj || !playerObj.id) {
+  if (!playerObj || !playerObj.id) {
     playerContainer.innerHTML = "<h3>Couldn't find data for this player!</h3>";
     return;
   }
@@ -56,13 +58,15 @@ export const renderSinglePlayer = (playerObj) => {
       </div>
       <p>Team: ${playerObj.team ? playerObj.team.name : 'Unassigned'}</p>
       <p>Breed: ${playerObj.breed}</p>
-      <img src="${playerObj.imageUrl}" alt="photo of ${playerObj.name} the puppy">
+      <img src="${playerObj.imageUrl}" alt="photo of ${
+    playerObj.name
+  } the puppy">
       <button id="see-all">Back to all players</button>
     </div>
   `;
 
   playerContainer.innerHTML = pupHTML;
-}
+};
 
 export const renderNewPlayerForm = () => {
   let formHTML = `
@@ -82,4 +86,4 @@ export const renderNewPlayerForm = () => {
       YOUR CODE HERE
     */
   });
-}
+};
